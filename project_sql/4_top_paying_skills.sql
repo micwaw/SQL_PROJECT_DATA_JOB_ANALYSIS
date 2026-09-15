675f92ace613a8a21da73b6546b3ef6fc32b0cef
micwaw <micwawrzen@gmail.com>
2026-09-15 11:26:01 +0200

    upload sql files

---
+++ project_sql/4_top_paying_skills.sql
@@ -0,0 +1,23 @@
+/*
+Answer: What are the top paying skills based on salary for data analyst jobs?
+- Look at the average salary associated with each skill for data analyst jobs.
+- Focus on roles with specified salaries, regardles of location
+- Why? It reveals how diffrent skills impact salary levels for Data Analyst positions and
+ helping job seekers prioritize skill development for higher earning potential.
+*/
+
+SELECT 
+ skills,
+  ROUND(AVG(salary_year_avg), 2) AS average_salary
+FROM job_postings_fact 
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+job_title_short = 'Data Analyst' 
+AND salary_year_avg IS NOT NULL
+AND job_work_from_home = true
+GROUP BY 
+skills
+ORDER BY 
+average_salary DESC
+LIMIT 25;
